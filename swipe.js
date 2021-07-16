fd15d6f9f88661a3cf42bb60d719b9659f055bb5
Francisco Lazcano <a01366080@itesm.mx>
2021-07-16 12:04:53 -0500

    version preliminar slideshow movil

--- swipe.js
+++ swipe.js
@@ -1,5 +1,6 @@
+var el=document.getElementById("mobile")
 
-var test=Hammer(document.getElementById("mobile"))
+var test= new Hammer(el)
 test.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 test.on('swipe',function(ev){
   alert(ev);
