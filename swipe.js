9fb84f7e25ccc23d26f134ad8b390977c531825f
Francisco Lazcano <a01366080@itesm.mx>
2021-07-16 12:29:36 -0500

    version preliminar slideshow movil

--- swipe.js
+++ swipe.js
@@ -1,7 +1,17 @@
-var el=document.getElementById("mobile")
-
-var test= new Hammer(el)
-test.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
-test.on('swipe',function(ev){
-  alert(ev);
+var current=0;
+const slides=[document.getElementById("niv1"),document.getElementById("niv2"),document.getElementById("niv3")];
+slides[1].style.display="none"
+slides[2].style.display="none"
+var slideshow= new Hammer(document.getElementById("mobile"))
+slideshow.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
+slideshow.on('swipe',function(ev){
+  if(ev.direction==2){
+    slides[current].style.display="none"
+    current+=1;
+    slides[current].style.display="inherit"
+  }else{
+    slides[current].style.display="none"
+    current-=1;
+    slides[current].style.display="inherit"
+  }
 });
